8bf948c1d11d046c8a94e6e34cdfc2dd543f1677
olegukr <oleg.poberezhets@gmail.com>
2024-10-31 14:22:45 +0100

    add call method in app.java for test

--- src/main/java/dev/proyect/inside_out/App.java
+++ src/main/java/dev/proyect/inside_out/App.java
@@ -1,13 +1,10 @@
 package dev.proyect.inside_out;
-
 import dev.proyect.inside_out.models.Emotion;
 
 public final class App {
-    private App() {
-    }
-
+  
     public static void main(String[] args) {
-        // Emotion emotionsList = new Emotion();
+        Emotion emotionsList = new Emotion();
         System.out.println("Hi");
         System.out.println(Emotion.getEmotions().toString());
         System.out.println(Emotion.getEmotionByIndex( 1));
