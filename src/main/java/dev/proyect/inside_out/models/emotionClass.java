5c07c88aa841cd1d182a579b5553ec0c1c8b5e1e
olegukr <oleg.poberezhets@gmail.com>
2024-10-31 13:31:30 +0100

    add class Emotion and 2 methods getEmotions, getEmotionByIndex

---
+++ src/main/java/dev/proyect/inside_out/models/emotionClass.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package dev.proyect.inside_out.models;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author olegpoberezhets
+ */
+public class emotionClass {
+
+    public static List<String> emociones = Arrays.asList(
+    "Alegría",
+        "Tristeza",
+        "Ira",
+        "Asco",
+        "Miedo",
+        "Ansiedad",
+        "Envidia",
+        "Vergüenza",
+        "Aburrimiento",
+        "Nostalgia"
+    );
+
+    
+
+
+}
