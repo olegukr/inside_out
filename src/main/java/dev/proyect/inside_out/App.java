62256136ad6bdb1057c3ab5684e5473d7e71e0bd
olegukr <oleg.poberezhets@gmail.com>
2024-10-31 14:35:08 +0100

    clear main method

--- src/main/java/dev/proyect/inside_out/App.java
+++ src/main/java/dev/proyect/inside_out/App.java
@@ -1,12 +1,10 @@
 package dev.proyect.inside_out;
-import dev.proyect.inside_out.models.Emotion;
 
 public final class App {
   
     public static void main(String[] args) {
-        Emotion emotionsList = new Emotion();
+
         System.out.println("Hi");
-        System.out.println(Emotion.getEmotions().toString());
-        System.out.println(Emotion.getEmotionByIndex( 1));
+
     }
 }
