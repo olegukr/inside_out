296b7b3005d81fcbc5ea05fa395ebbbfcc63011d
abdias labrador <abdiaslabrador@gmail.com>
2024-10-31 14:39:01 +0100

    feat: momentController class without define each function

---
+++ src/main/java/dev/proyect/inside_out/controller/momentController.java
@@ -0,0 +1,32 @@
+package dev.proyect.inside_out.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
+import dev.proyect.inside_out.models.Moment;
+
+public class momentController {
+    
+    List<Moment> moments = new ArrayList<Moment>();
+
+    // public void addMoment(Moment moment){
+        
+    // }
+    // public List<Moment> getMoments(){
+
+    // }
+    // public List<Moment> deleteMoment(Moment moment){
+
+    // }
+    // public List<Moment> getFilterByEmotion() {
+        
+    // }
+    // public List<Moment> getFilterByDate(Date date){
+
+    // }
+
+    // updateMomentDate( Date) void {
+
+    // }
+}
